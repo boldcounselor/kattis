84234509462d260610a2cfffe0a7a3506086e1d3
Conrad <conradsoon@gmail.com>
2020-01-09 14:04:40 +0800

    Added some more.

--- anagramcounting.py
+++ anagramcounting.py
@@ -1,32 +1,15 @@
-#include <bits/stdc++.h>
-#include <string>
-using namespace std;
+import sys
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    string word;
-    while(getline(cin,word)){
-        unordered_map<char,int> chars;
-        long double perms = 1;
-        int noc = word.length();
-        for(char& c : word ){
-            if(chars.count(c)==false){
-                chars[c]=1;
-            }
-            else{
-                chars[c]++;
-            }
-        }
-        for(auto x : chars){
-            for(int i = 1; i<=x.second; i++){
-                perms/=i;
-                perms*=noc;
-                noc--;
-            }
-        }
-        cout << fixed << perms << "\n";
-    }
-    return 0;
-}
+def main():
+    for line in sys.stdin:
+        line.rstrip()
+        perms = factorial(line.len())
+        chars = dict()
+        for char in line:
+            if char in chars:
+                chars[char] = 1
+            else:
+                chars[char] += 1
+        for char, no in chars.items():
+            perms /= no
+    print(perms)
